27ef73d2644ab6ac634158cc09bc817c7a3752a7
SnowSurfer <drinkaziu@gmail.com>
2018-12-13 10:57:06 -0500

    initial heroku commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} - ${req.url}`;
+  fs.appendFileSync('server.log', log + '\n', (err) => {
+    console.log('Unable to append to server.log');
+  });
+  console.log(log);
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('currentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('scream', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    greeting: 'Welcome to Some Website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
